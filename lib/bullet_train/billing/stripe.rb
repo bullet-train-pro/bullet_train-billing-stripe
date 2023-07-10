2c400de259af52bc7164c9be41e2e7a29d923aca
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-10 16:42:51 +0200

    Use Teams::Base load hook to insert functionality
    This work is dependent on bullet-train-co/bullet_train-core#335 being
    shipped.
    
    Then we're just grabbing the bullet_train-billing-stripe specific extensions and pulling them in to our
    repo. Note: we can skip the surrounding `defined?(Billing::Stripe::Subscription)` checks, since we already know its enabled
    by the fact that we're loading the very library.

--- lib/bullet_train/billing/stripe.rb
+++ lib/bullet_train/billing/stripe.rb
@@ -4,7 +4,15 @@ require "bullet_train/billing/stripe/engine"
 module BulletTrain
   module Billing
     module Stripe
-      # Your code goes here...
+      module Teams
+        module Base
+          extend ActiveSupport::Concern
+
+          included do
+            has_many :billing_stripe_subscriptions, class_name: "Billing::Stripe::Subscription", dependent: :destroy, foreign_key: :team_id
+          end
+        end
+      end
     end
   end
 end
@@ -12,3 +20,5 @@ end
 def stripe_billing_enabled?
   ENV["STRIPE_SECRET_KEY"].present?
 end
+
+ActiveSupport.on_load(:bullet_train_teams_base) { include BulletTrain::Billing::Stripe::Teams::Base }
