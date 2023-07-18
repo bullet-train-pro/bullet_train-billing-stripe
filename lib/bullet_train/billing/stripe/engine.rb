f3e31f8e7c407e8e9e4b87377c281a30824a9b27
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-18 23:31:28 +0200

    Hook into BulletTrain::Billing.provider_subscription_attributes

--- lib/bullet_train/billing/stripe/engine.rb
+++ lib/bullet_train/billing/stripe/engine.rb
@@ -2,6 +2,11 @@ module BulletTrain
   module Billing
     module Stripe
       class Engine < ::Rails::Engine
+        initializer "bullet_train-billing.integrate" do
+          config.after_initialize do
+            BulletTrain::Billing.provider_subscription_attributes << :stripe_subscription_id
+          end
+        end
       end
     end
   end
